c732ce45c0108c16ae7f8b9c75809196e8cca1db
raquelramor92 <61478827+raquelramor92@users.noreply.github.com>
2020-02-28 12:06:20 -0600

    Update Tarea_micros.py
    Correcciones de flake8

--- Tarea_micros.py
+++ Tarea_micros.py
@@ -1,21 +1,29 @@
 
-#Error 1 para tipo de dato recibido incorrecto
+# Error 1 para tipo de dato recibido incorrecto
+
+
 def Error1():
     return "Error 1"
-#Error 2 para función 1, dato ya se encuentra en mayúscula
+
+# Error 2 para función 1, dato ya se encuentra en mayúscula
+
+
 def Error2():
     return "Error 2"
-#Error 3 para función 3, no natural
+
+# Error 3 para función 3, no natural
+
+
 def Error3():
     return "Error 3"
 
 
 def PrimeraFuncion (x):
     for i in x:
-        if i.isupper() == True:
+        if i.isupper() is True:
             return Error2()
             break
-        if i.isdigit() == True:
+        if i.isdigit() is True:
             return Error1()
             break
     x = x.upper()
@@ -25,14 +33,15 @@ def PrimeraFuncion (x):
 def SegundaFuncion(x):
     y = "False"
     for i in x:
-        if i.isdigit() == True:
+        if i.isdigit() is True:
             return Error1()
             break
         
         if i == "w":
             y = "True"
     return y
-            
+
+
 def TerceraFuncion(x,y):
     xc = str(x)
     yc = str(y)
@@ -45,6 +54,3 @@ def TerceraFuncion(x,y):
     if Resta < 0:
         return Error3()
     return Resta
-
-    
-
