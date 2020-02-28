db94763bc7da480740beb8fec11415e960f9ad4a
raquelramor92 <61478827+raquelramor92@users.noreply.github.com>
2020-02-28 00:24:10 -0600

    Corrección de errores
    Código corregido según indicaciones de flake8 corrido sobre el código localmente

--- Codigo_errores_flake8.py
+++ Codigo_errores_flake8.py
@@ -1,16 +1,13 @@
 
 # Programa con errores para probarlo en flake8
+
+
 def suma(a,b,c):
-    listasuma =    [a,b,c]
-
-    aa= str(a)
-
-    bb=   str(b)
-
-    cc= str(c)
-    
-    if aa.isdigit() == False or bb.isdigit() == False or cc.isdigit() == False:
-          return " Alguno de los argumentos no es un número, vuelva a intentar"
+    listasuma = [a, b, c]
+    aa = str(a)
+    bb = str(b)
+    cc = str(c) 
+    if aa.isdigit() is False or bb.isdigit() is False or cc.isdigit() is False:
+        return " Alguno de los argumentos no es un número, vuelva a intentar"
 
     return sum(listasuma)
-  
